a7524c95bda94695097af7569538f690a122096d
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-09 09:39:43 +0100

    fc

---
+++ src/main/java/org/youcode/majesticcup/MajesticCupApplication.java
@@ -0,0 +1,14 @@
+package org.youcode.majesticcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajesticCupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajesticCupApplication.class, args);
+		System.out.println("MajesticCup Application Started");
+	}
+
+}

ba6bb6e6cdc4014ce1cf8260b541f0bfd6af80c7
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-09 17:14:29 +0100

    feat: added Static as a Sub_document

--- src/main/java/org/youcode/majesticcup/model/sub_document/Static.java
+++ src/main/java/org/youcode/majesticcup/model/sub_document/Static.java
@@ -1,4 +1,19 @@
 package org.youcode.majesticcup.model.sub_document;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Static {
+    private ObjectId playerId;
+    private int goals;
+    private int assists;
+    private int yellowCards;
+    private int redCards;
 }

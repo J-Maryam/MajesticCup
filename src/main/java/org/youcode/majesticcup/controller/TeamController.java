c3d1b9dd21d2147b33c2acd863b919ae94c5f1c1
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-13 02:21:57 +0100

    feat(controller): add updateTeam endpoint
    - Implemented PUT /api/admin/teams/{id} to update an existing team by ID.
    
    - Validates the request payload using @Valid.
    
    - Returns the updated team with a 200 OK HTTP status.

--- src/main/java/org/youcode/majesticcup/controller/TeamController.java
+++ src/main/java/org/youcode/majesticcup/controller/TeamController.java
@@ -2,12 +2,10 @@ package org.youcode.majesticcup.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.youcode.majesticcup.dto.team.TeamRequestDTO;
 import org.youcode.majesticcup.dto.team.TeamResponseDTO;
 import org.youcode.majesticcup.service.TeamService;
@@ -23,5 +21,13 @@ public class TeamController {
     public ResponseEntity<TeamResponseDTO> createTeam(@RequestBody @Valid TeamRequestDTO request) {
         TeamResponseDTO createdTeam = teamService.createTeam(request);
         return new ResponseEntity<>(createdTeam, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TeamResponseDTO> updateTeam(
+            @PathVariable ObjectId id,
+            @RequestBody @Valid TeamRequestDTO request) {
+        TeamResponseDTO updatedTeam = teamService.updateTeam(id, request);
+        return ResponseEntity.ok(updatedTeam);
     }
 }

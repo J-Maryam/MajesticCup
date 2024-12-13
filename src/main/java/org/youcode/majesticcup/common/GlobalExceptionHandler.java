f90b3c6fa6aafc8a9729e2b689a86c79663c0cd8
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-13 02:05:22 +0100

    feat (GlobalHanlderException): added GlobalHandlerException to handle the exceptions

---
+++ src/main/java/org/youcode/majesticcup/common/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package org.youcode.majesticcup.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.youcode.majesticcup.common.exceptions.BadRequestException;
+import org.youcode.majesticcup.common.exceptions.ConflictException;
+import org.youcode.majesticcup.common.exceptions.EntityCreationException;
+import org.youcode.majesticcup.common.exceptions.EntityNotFoundException;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+//    @ExceptionHandler(DataIntegrityViolationException.class)
+//    public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
+//        String message = String.format("Message: %s | URI: %s", ex.getMessage(), request.getRequestURI());
+//        return buildErrorResponse(HttpStatus.CONFLICT, message);
+//    }
+
+    @ExceptionHandler(EntityCreationException.class)
+    public ResponseEntity<ErrorResponse> handleEntityCreationException(EntityCreationException ex) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<ErrorResponse> handleConflictException(ConflictException ex) {
+       return buildErrorResponse(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String message) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                message
+        );
+        return new ResponseEntity<>(errorResponse, status);
+    }
+}

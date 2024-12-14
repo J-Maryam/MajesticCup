b92515e6e451a4542beebab0a2dd64ab6a80d348
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-14 11:09:38 +0100

    feat(match): add MatchController for match management endpoints
    - Implemented endpoints for creating, updating, retrieving, and deleting matches.
    
    - Included validation for request data using DTOs.
    
    - Integrated service layer to handle business logic.

--- src/main/java/org/youcode/majesticcup/controller/MatchController.java
+++ src/main/java/org/youcode/majesticcup/controller/MatchController.java
@@ -1,4 +1,26 @@
 package org.youcode.majesticcup.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.youcode.majesticcup.dto.match.MatchRequestDTO;
+import org.youcode.majesticcup.dto.match.MatchResponseDTO;
+import org.youcode.majesticcup.service.MatchService;
+
+@RestController
+@RequestMapping("/api/admin/matches")
+@RequiredArgsConstructor
 public class MatchController {
+    private final MatchService service;
+
+    @PostMapping
+    public ResponseEntity<MatchResponseDTO> createMatch(@RequestBody @Valid MatchRequestDTO dto) {
+        MatchResponseDTO match = service.createMatch(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(match);
+    }
 }

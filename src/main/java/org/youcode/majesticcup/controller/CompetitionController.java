e98ce0d7c8c9450a52a02461f3562b9050cd797b
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-15 00:57:15 +0100

    feat(controller): add endpoint for competition creation
    Implemented a POST endpoint in the CompetitionController to handle competition creation. It receives a CompetitionRequestDTO, passes it to the service layer, and returns the created CompetitionResponseDTO.

--- src/main/java/org/youcode/majesticcup/controller/CompetitionController.java
+++ src/main/java/org/youcode/majesticcup/controller/CompetitionController.java
@@ -1,4 +1,27 @@
 package org.youcode.majesticcup.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.youcode.majesticcup.dto.competition.CompetitionRequestDTO;
+import org.youcode.majesticcup.dto.competition.CompetitionResponseDTO;
+import org.youcode.majesticcup.service.CompetitionService;
+
+@RestController
+@RequestMapping("/api/admin/competitions")
+@RequiredArgsConstructor
 public class CompetitionController {
+
+    private final CompetitionService service;
+
+    @PostMapping
+    public ResponseEntity<CompetitionResponseDTO> createCompetition(@RequestBody @Valid CompetitionRequestDTO dto) {
+        CompetitionResponseDTO response = service.createCompetition(dto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }

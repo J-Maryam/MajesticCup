b43b112149368a638eb653ff87c223f4cf88e79f
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-15 15:55:31 +0100

    feat(round): Added findByCompetitionId method to retrieve all rounds for a competition

--- src/main/java/org/youcode/majesticcup/repository/CompetitionRepository.java
+++ src/main/java/org/youcode/majesticcup/repository/CompetitionRepository.java
@@ -1,4 +1,10 @@
 package org.youcode.majesticcup.repository;
 
-public interface CompetitionRepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.youcode.majesticcup.model.collections.Competition;
+
+@Repository
+public interface CompetitionRepository extends MongoRepository<Competition, ObjectId> {
 }
--- src/main/java/org/youcode/majesticcup/repository/RoundRepository.java
+++ src/main/java/org/youcode/majesticcup/repository/RoundRepository.java
@@ -1,4 +1,14 @@
 package org.youcode.majesticcup.repository;
 
-public interface RoundRepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.youcode.majesticcup.model.collections.Competition;
+import org.youcode.majesticcup.model.collections.Round;
+
+import java.util.List;
+
+@Repository
+public interface RoundRepository extends MongoRepository<Round, ObjectId> {
+    List<Round> findByCompetitionId(Competition competition);
 }

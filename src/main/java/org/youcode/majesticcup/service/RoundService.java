4fdc1b2955b4177c2cdccfd831e87d2e9b8142a9
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-15 15:45:42 +0100

    feat: Implement RoundService for round creation
    - Added createRound method to handle round creation.
    
    - Validates competition existence before creating the round.
    
    - Fetches and validates match IDs from the database.
    
    - Maps created rounds to RoundResponseDTO for response.

--- src/main/java/org/youcode/majesticcup/service/RoundService.java
+++ src/main/java/org/youcode/majesticcup/service/RoundService.java
@@ -1,4 +1,8 @@
 package org.youcode.majesticcup.service;
 
+import org.youcode.majesticcup.dto.round.RoundRequestDTO;
+import org.youcode.majesticcup.dto.round.RoundResponseDTO;
+
 public interface RoundService {
+    RoundResponseDTO createRound(RoundRequestDTO dto);
 }
--- src/main/java/org/youcode/majesticcup/service/impl/RoundServiceImpl.java
+++ src/main/java/org/youcode/majesticcup/service/impl/RoundServiceImpl.java
@@ -1,4 +1,51 @@
-package org.youcode.majesticcup.service.impl;
+    package org.youcode.majesticcup.service.impl;
 
-public class RoundServiceImpl {
-}
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.stereotype.Service;
+    import org.springframework.transaction.annotation.Transactional;
+    import org.youcode.majesticcup.common.exceptions.EntityNotFoundException;
+    import org.youcode.majesticcup.dto.round.RoundRequestDTO;
+    import org.youcode.majesticcup.dto.round.RoundResponseDTO;
+    import org.youcode.majesticcup.mapper.RoundMapper;
+    import org.youcode.majesticcup.model.collections.Competition;
+    import org.youcode.majesticcup.model.collections.Match;
+    import org.youcode.majesticcup.model.collections.Round;
+    import org.youcode.majesticcup.repository.CompetitionRepository;
+    import org.youcode.majesticcup.repository.MatchRepository;
+    import org.youcode.majesticcup.repository.RoundRepository;
+    import org.youcode.majesticcup.repository.TeamRepository;
+    import org.youcode.majesticcup.service.RoundService;
+
+    import java.util.List;
+    import java.util.stream.Collectors;
+
+    @Service
+    @Transactional
+    @RequiredArgsConstructor
+    public class RoundServiceImpl implements RoundService {
+
+        private final RoundRepository repository;
+        private final RoundMapper mapper;
+        private final MatchRepository matchRepository;
+        private final TeamRepository teamRepository;
+        private final CompetitionRepository competitionRepository;
+
+        @Override
+        public RoundResponseDTO createRound(RoundRequestDTO dto) {
+            Competition competition = competitionRepository.findById(dto.competitionId())
+                    .orElseThrow(() -> new EntityNotFoundException("Competition not found with ID: " + dto.competitionId()));
+
+            List<Match> matches = dto.matches().stream()
+                    .map(matchId -> matchRepository.findById((matchId))
+                            .orElseThrow(() -> new EntityNotFoundException("Match not found with ID: " + matchId)))
+                    .collect(Collectors.toList());
+
+            Round round = new Round();
+            round.setRoundNumber(dto.roundNumber());
+            round.setCompetitionId(competition);
+            round.setMatches(matches);
+
+            Round savedRound = repository.save(round);
+            return mapper.toDto(savedRound);
+        }
+    }

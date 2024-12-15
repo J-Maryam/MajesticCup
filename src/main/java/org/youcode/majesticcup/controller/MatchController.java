6e5e6e760d2f0eb5b5dc628727c8709d76aca204
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-15 21:55:08 +0100

    feat(controller): add endpoint for registering match results
    - Created an API endpoint to handle match result registration.
    
    - Integrated validation for input DTO.
    
    - Delegated business logic to the match service.
    
    - Returned appropriate HTTP status codes and response body.

--- src/main/java/org/youcode/majesticcup/controller/MatchController.java
+++ src/main/java/org/youcode/majesticcup/controller/MatchController.java
@@ -2,12 +2,11 @@ package org.youcode.majesticcup.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.youcode.majesticcup.dto.MatchResultDTO;
 import org.youcode.majesticcup.dto.match.MatchRequestDTO;
 import org.youcode.majesticcup.dto.match.MatchResponseDTO;
 import org.youcode.majesticcup.service.MatchService;
@@ -22,5 +21,14 @@ public class MatchController {
     public ResponseEntity<MatchResponseDTO> createMatch(@RequestBody @Valid MatchRequestDTO dto) {
         MatchResponseDTO match = service.createMatch(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(match);
+    }
+
+    @PutMapping("/{id}/result")
+    public ResponseEntity<MatchResponseDTO> updateMatchResult(
+            @PathVariable("id") ObjectId matchId,
+            @RequestBody @Valid MatchResultDTO matchResultDTO
+    ) {
+        MatchResponseDTO updatedMatch = service.registerMatchResult(matchId, matchResultDTO);
+        return ResponseEntity.ok(updatedMatch);
     }
 }

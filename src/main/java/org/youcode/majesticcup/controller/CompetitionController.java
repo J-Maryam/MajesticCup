3b85ca3365a8b1a0f5a1831c799caf13f51c7a60
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-15 01:13:54 +0100

    feat: add GET endpoint to fetch competition by ID

--- src/main/java/org/youcode/majesticcup/controller/CompetitionController.java
+++ src/main/java/org/youcode/majesticcup/controller/CompetitionController.java
@@ -2,12 +2,10 @@ package org.youcode.majesticcup.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.youcode.majesticcup.dto.competition.CompetitionRequestDTO;
 import org.youcode.majesticcup.dto.competition.CompetitionResponseDTO;
 import org.youcode.majesticcup.service.CompetitionService;
@@ -23,5 +21,11 @@ public class CompetitionController {
     public ResponseEntity<CompetitionResponseDTO> createCompetition(@RequestBody @Valid CompetitionRequestDTO dto) {
         CompetitionResponseDTO response = service.createCompetition(dto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CompetitionResponseDTO> getCompetitionById(@PathVariable ObjectId id) {
+        CompetitionResponseDTO response = service.getCompetitionById(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }

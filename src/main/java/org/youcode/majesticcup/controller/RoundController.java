f66f08a7cc208e89d1ee9cc5121b7b6d64ed27bd
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-15 15:47:12 +0100

    feat: Add RoundController for handling round-related API endpoints
    - Implemented POST endpoint for creating rounds.
    
    - Connected RoundController to RoundService for logic handling.
    
    - Added basic route mappings for round operations.
    
    - Ensures clear separation between controller and service layers.

--- src/main/java/org/youcode/majesticcup/controller/RoundController.java
+++ src/main/java/org/youcode/majesticcup/controller/RoundController.java
@@ -1,4 +1,26 @@
 package org.youcode.majesticcup.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.youcode.majesticcup.dto.round.RoundRequestDTO;
+import org.youcode.majesticcup.dto.round.RoundResponseDTO;
+import org.youcode.majesticcup.service.RoundService;
+
+@RestController
+@RequestMapping("/api/admin/rounds")
+@RequiredArgsConstructor
 public class RoundController {
+    private final RoundService service;
+
+    @PostMapping
+    public ResponseEntity<RoundResponseDTO> createRound(@RequestBody @Valid RoundRequestDTO requestDTO) {
+        RoundResponseDTO createdRound = service.createRound(requestDTO);
+        return ResponseEntity.ok(createdRound);
+    }
+
 }

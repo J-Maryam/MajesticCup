a4b39afaa2c7df6a3cfbf9912390cef6d3717de6
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-23 14:04:23 +0100

    feat: implement addRole method in RoleServiceImpl

--- src/main/java/org/youcode/majesticcup/service/impl/RoleServiceImpl.java
+++ src/main/java/org/youcode/majesticcup/service/impl/RoleServiceImpl.java
@@ -1,4 +1,23 @@
 package org.youcode.majesticcup.service.impl;
 
-public class RoleServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.youcode.majesticcup.model.collections.Role;
+import org.youcode.majesticcup.repository.RoleRepository;
+import org.youcode.majesticcup.service.RoleService;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepository repository;
+
+    @Override
+    public Role addRole(Role role) {
+//        if (role.getName() == null || role.getName().isBlank()) {
+//            throw new IllegalArgumentException("Role's name cannot be null or blank.");
+//        }
+        return repository.save(role);
+    }
 }

8927a9f81995966cb5b4e6d6e443c1809c9ac580
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-23 13:57:21 +0100

    refactor(repository): added existByUsername method

--- src/main/java/org/youcode/majesticcup/repository/UserRepository.java
+++ src/main/java/org/youcode/majesticcup/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package org.youcode.majesticcup.repository;
 
-public interface UserRepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.youcode.majesticcup.model.collections.AppUser;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<AppUser, ObjectId> {
+    Optional<AppUser> findByUsername(String username);
 }

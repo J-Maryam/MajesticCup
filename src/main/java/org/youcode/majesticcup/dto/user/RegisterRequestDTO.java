f65256f3d54e7fe59a4bb5fc6a972f44b7dc18fa
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-23 13:43:49 +0100

    refactor: Added Role in User dto

--- src/main/java/org/youcode/majesticcup/dto/user/RegisterRequestDTO.java
+++ src/main/java/org/youcode/majesticcup/dto/user/RegisterRequestDTO.java
@@ -3,14 +3,14 @@ package org.youcode.majesticcup.dto.user;
 import jakarta.validation.constraints.NotBlank;
 import org.bson.types.ObjectId;
 
-public record UserRequestDTO(
+public record RegisterRequestDTO(
         @NotBlank(message = "Username cannot be blank.")
         String username,
 
         @NotBlank(message = "Password cannot be blank.")
         String password,
 
-        @NotBlank(message = "Role cannot be blank.")
+//        @NotBlank(message = "Role cannot be blank.")
         ObjectId role
 ) {}
 
--- src/main/java/org/youcode/majesticcup/dto/user/UserResponseDTO.java
+++ src/main/java/org/youcode/majesticcup/dto/user/UserResponseDTO.java
@@ -1,11 +1,12 @@
 package org.youcode.majesticcup.dto.user;
 
 import org.bson.types.ObjectId;
+import org.youcode.majesticcup.model.collections.Role;
 
 public record UserResponseDTO(
         ObjectId id,
         String username,
-        ObjectId role
+        Role role
 ) {}
 
 

ef4d0f732123d4ff297258c70264b0976ab28c3d
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-23 14:05:20 +0100

    fea(service): added register and verify methods in UserService interface

--- src/main/java/org/youcode/majesticcup/service/UserService.java
+++ src/main/java/org/youcode/majesticcup/service/UserService.java
@@ -1,4 +1,13 @@
 package org.youcode.majesticcup.service;
 
+import org.youcode.majesticcup.dto.user.LoginRequestDTO;
+import org.youcode.majesticcup.dto.user.RegisterRequestDTO;
+import org.youcode.majesticcup.dto.user.UserResponseDTO;
+import org.youcode.majesticcup.model.collections.AppUser;
+
 public interface UserService {
+    UserResponseDTO register(RegisterRequestDTO dto);
+
+//        AppUser authenticate(LoginRequestDTO dto);
+    String verify(LoginRequestDTO user);
 }

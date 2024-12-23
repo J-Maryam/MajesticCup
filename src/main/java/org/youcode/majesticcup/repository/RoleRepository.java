c95ab49e960d61c4908815975820fe25d3afedd3
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-23 13:56:21 +0100

    refactor(repository): added findByName method

--- src/main/java/org/youcode/majesticcup/repository/RoleRepository.java
+++ src/main/java/org/youcode/majesticcup/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package org.youcode.majesticcup.repository;
 
-public interface RoleRepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.youcode.majesticcup.model.collections.Role;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends MongoRepository<Role, ObjectId> {
+    Optional<Role> findByName(String name);
 }
